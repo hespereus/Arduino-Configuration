7f997f034b9fa5d7452c287dec478a11f1c4e5e8
Abram M. Unabia <unabiabram@outlook.com>
2020-01-09 12:01:07 +0800

    Nerdaqll

--- biquad.h
+++ biquad.h
@@ -5,18 +5,6 @@
 * Description: Provides a template for implementing IIR filters as a cacade
 * of second-order sections, aka, "biquads".
 *
-* by Grant R. Griffin
-* Copyright 2007-2010, Iowegian International Corporation
-* (http:**www.iowegian.com)
-*
-*                          The Wide Open License (WOL)
-*
-* Permission to use, copy, modify, distribute and sell this software and its
-* documentation for any purpose is hereby granted without fee, provided that
-* the above copyright notice and this license appear in all source copies. 
-* THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT EXPRESS OR IMPLIED WARRANTY OF
-* ANY KIND. See http:**www.dspguru.com*wol.htm for more information.
-*
 *****************************************************************************/
 
 #define BIQUAD_NCOEFF 6
